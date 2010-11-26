57d55e6119d42535d4aa7b2eb240417f4f51c0b8
Ben Foxall <benfoxall@gmail.com>
2010-11-26 13:08:09 +0000

    new landing

--- dev.js
+++ dev.js
@@ -8,9 +8,9 @@ var error = require('./util').error
 
 var app = express.createServer();
 
-app.get('/',function(req,res){
-	res.send('<!DOCTYPE html><html><head><title>trsslate</title><style type="text/css" media="screen">body{font-family:Helvetica,Arial,sans-serif;font-size:1.5em;}label{display:block;margin-top:1em;}input{font-size:1.4em;display:inline;height:40px;border:1px solid #ccc;}#u{width:20em;}</style></head><body><h1>Trsslate</h1><form method="get" action="f"><p><label for="u">Feed URL</label><input type="url" name="u" value="" id="u" placeholder="http://yoursite/feed.xml"><label for="s">Selector</label><input type="text" name="s" id="s" value="" placeholder="#content > img"><input type="submit" value="&raquo;"></p></form></body></html>');
-})
+app.configure(function(){
+    app.use(express.staticProvider(__dirname + '/public'));
+});
 
 app.get('/f', function(req, res){
 		uri = req.query['u'];
